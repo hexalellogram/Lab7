e0d548f6f357937dc13c3e1c2d2aa3a2cbc2647b
Bryce Tsuyuki <btsuyuki1@gmail.com>
2021-05-16 21:54:07 -0700

    listeners, popstate, event listener to load entries

--- scripts/script.js
+++ scripts/script.js
@@ -1,35 +1,42 @@
 // script.js
 
-import { router } from './router.js'; // Router imported so you can use it to manipulate your SPA app here
+import { router } from "./router.js"; // Router imported so you can use it to manipulate your SPA app here
 const setState = router.setState;
 
 // Make sure you register your service worker here too
 
+// handle popstate for history
+
+window.onpopstate = function (event) {
+  setState(event.state);
+};
+
 // set listeners for header text and settings icon
 
-const headerText = document.querySelector('header > h1');
-headerText.onclick = function() {
-  router.setState('home');
+const headerText = document.querySelector("header > h1");
+headerText.onclick = function () {
+  router.setState("home");
 };
 
-const gear = document.querySelector('header > img');
-gear.onclick = function() {
-  router.setState('settings');
+const gear = document.querySelector("header > img");
+gear.onclick = function () {
+  router.setState("settings");
 };
 
-
-document.addEventListener('DOMContentLoaded', () => {
-  fetch('https://cse110lab6.herokuapp.com/entries')
-    .then(response => response.json())
-    .then(entries => {
-      entries.forEach((entry, num) => {
-        entry.num = num;
-        let newPost = document.createElement('journal-entry');
+document.addEventListener("DOMContentLoaded", () => {
+  fetch("https://cse110lab6.herokuapp.com/entries")
+    .then((response) => response.json())
+    .then((entries) => {
+      let index = 1;
+      entries.forEach((entry) => {
+        entry.num = index;
+        let newPost = document.createElement("journal-entry");
         newPost.entry = entry;
-        newPost.onclick = function() {
-          router.setState('entry', entry);
+        newPost.onclick = function () {
+          router.setState("entry", entry);
         };
-        document.querySelector('main').appendChild(newPost);
+        document.querySelector("main").appendChild(newPost);
+        index++;
       });
     });
 });
